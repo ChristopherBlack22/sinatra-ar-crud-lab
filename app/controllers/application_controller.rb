e7dce32a1e372936c7ae9139df9a9655d35ab92c
Christopher <christopher.black.2209@googlemail.com>
2021-08-24 00:07:03 +0100

    commit tree f7d4a876a35bd58cccec77dc1cbacf855429e124

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,46 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get "/" do
+    redirect to "/articles"
+  end 
+
+  get "/articles/new" do
+    erb :new
+  end 
+
+  post "/articles" do
+    title = params[:title]
+    content = params[:content]
+    article = Article.create(title: title, content: content)
+    redirect to "/articles/#{article.id}"
+  end 
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
   end
+
+  get "/articles/:id" do
+    @article = Article.find(params[:id].to_i)
+    erb :show
+  end 
+
+  get "/articles/:id/edit" do
+    @article = Article.find(params[:id].to_i)
+    erb :edit
+  end 
+
+  patch "/articles/:id" do
+    article = Article.find(params[:id])
+
+    article.update(title: params[:title], content: params[:content])
+    redirect to "/articles/#{article.id}"
+  end 
+
+  delete "/articles/:id" do
+    Article.destroy(params[:id].to_i)
+    redirect to "/articles"
+  end 
+
 end
